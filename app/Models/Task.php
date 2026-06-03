8935ae6c7cb2b5b3bf5da0ba1a7d3ce88830fa72
Ivanna Maulida <ivanna@gmail.com>
2026-06-03 10:42:25 +0700

    docs: add PHPDoc comments to Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Task Model
+ *
+ * Merepresentasikan satu item tugas dalam aplikasi TaskFlow.
+ *
+ * @property int         $id
+ * @property string      $title        Judul tugas (wajib, min 3 karakter)
+ * @property string|null $description  Deskripsi detail tugas
+ * @property string|null $deadline     Tanggal batas waktu (format Y-m-d)
+ * @property bool        $is_done      Status penyelesaian tugas
+ * @property string      $priority     Prioritas: low | medium | high
+ */
 class Task extends Model
 {
+    /**
+     * Kolom yang boleh diisi secara massal (mass assignment).
+     */
     protected $fillable = [
         'title',
         'description',
